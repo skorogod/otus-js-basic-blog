e5dbd138376f0b5f7a82be69a51dda9d26f20ffe
Sergey <mail@vasya.com>
2023-10-21 03:05:29 +0300

    carousel bugs fixed

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -1,8 +1,9 @@
 function Carousel(element, interval = 2000) {
-  window.addEventListener('load', function () {
+  window.addEventListener("load", function () {
+    let intervalId;
     this.interval = interval;
 
-    const carouselItems = element.querySelectorAll('.carousel__item');
+    const carouselItems = element.querySelectorAll(".carousel__item");
 
     const itemWidth = element.clientWidth;
 
@@ -10,24 +11,24 @@ function Carousel(element, interval = 2000) {
       el.style.minWidth = `${itemWidth}px`;
     });
 
-    this.items = document.querySelectorAll('.carousel__item');
-    this.btnNext = document.querySelector('.carousel__btn.next__btn');
-    this.btnPrev = document.querySelector('.carousel__btn.prev__btn');
+    this.items = document.querySelectorAll(".carousel__item");
+    this.btnNext = document.querySelector(".carousel__btn.next__btn");
+    this.btnPrev = document.querySelector(".carousel__btn.prev__btn");
 
     let index = 0;
 
-    this.items[1].classList.add('next');
-    this.items[this.items.length - 1].classList.add('prev');
+    this.items[1].classList.add("next");
+    this.items[this.items.length - 1].classList.add("prev");
 
     this.prev = function () {
       if (index < this.items.length - 1) {
-        this.items[index + 1].classList.remove('next');
+        this.items[index + 1].classList.remove("next");
       } else {
-        this.items[0].classList.remove('next');
+        this.items[0].classList.remove("next");
       }
 
-      this.items[index].classList.add('next');
-      this.items[index].classList.remove('active');
+      this.items[index].classList.add("next");
+      this.items[index].classList.remove("active");
 
       index -= 1;
 
@@ -35,25 +36,25 @@ function Carousel(element, interval = 2000) {
         index = this.items.length - 1;
       }
 
-      this.items[index].classList.remove('prev');
-      this.items[index].classList.add('active');
+      this.items[index].classList.remove("prev");
+      this.items[index].classList.add("active");
 
       if (index > 0) {
-        this.items[index - 1].classList.add('prev');
+        this.items[index - 1].classList.add("prev");
       } else {
-        this.items[this.items.length - 1].classList.add('prev');
+        this.items[this.items.length - 1].classList.add("prev");
       }
     };
 
     this.next = function () {
       if (index > 0) {
-        this.items[index - 1].classList.remove('prev');
+        this.items[index - 1].classList.remove("prev");
       } else {
-        this.items[this.items.length - 1].classList.remove('prev');
+        this.items[this.items.length - 1].classList.remove("prev");
       }
 
-      this.items[index].classList.remove('active');
-      this.items[index].classList.add('prev');
+      this.items[index].classList.remove("active");
+      this.items[index].classList.add("prev");
 
       index += 1;
 
@@ -61,19 +62,31 @@ function Carousel(element, interval = 2000) {
         index = 0;
       }
 
-      this.items[index].classList.remove('next');
-      this.items[index].classList.add('active');
+      this.items[index].classList.remove("next");
+      this.items[index].classList.add("active");
 
       if (index < this.items.length - 1) {
-        this.items[index + 1].classList.add('next');
+        this.items[index + 1].classList.add("next");
       } else {
-        this.items[0].classList.add('next');
+        this.items[0].classList.add("next");
       }
     };
 
-    this.btnNext.addEventListener('click', this.next.bind(this));
-    this.btnPrev.addEventListener('click', this.prev.bind(this));
+    function onNextClick() {
+      clearInterval(intervalId);
+      this.next.call(this);
+      intervalId = setInterval(this.next.bind(this), interval);
+    }
 
-    setInterval(this.next.bind(this), this.interval);
+    function onPrevClick() {
+      clearInterval(intervalId);
+      this.prev.call(this);
+      intervalId = setInterval(this.next.bind(this), interval);
+    }
+
+    this.btnNext.addEventListener("click", onNextClick.bind(this));
+    this.btnPrev.addEventListener("click", onPrevClick.bind(this));
+
+    intervalId = setInterval(this.next.bind(this), this.interval);
   });
 }
